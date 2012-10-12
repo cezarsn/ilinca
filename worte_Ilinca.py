254cebeed4d2f1c90d33119b9e66b88bd56f9170
Cezar Spatariu Neagu <cezar.spatariu@gmail.com>
2012-10-12 17:12:48 +0200

    Implementata partea de cautare in fisier

--- worte_Ilinca.py
+++ worte_Ilinca.py
@@ -1,41 +1,57 @@
+
 import tkinter
 import random
 
 
+
+
 class myGui:
+    
     def __init__(self):
+        fisier = "cuvinte.txt"
+        self.cuvinte = self.genereaza_cuvinte(3, fisier)
         self.main_window = tkinter.Tk()
         self.label1 = tkinter.Label(master = self.main_window, text = "die Meerjungfrau",font=("Helvetica Bold", 120))
         self.label1.pack( side = "top" )
         self.button1 = tkinter.Button(master = self.main_window, text = "Next", command = self.change_the_word)
         self.button1.pack (side = "top")
 
+    def genereaza_cuvinte(self, lungime, fisier):
+        '''
+
+        Procedura care incarca dint-un fisier  cuvintele si genereaza o lista de cuvinte de o anumita lungime
+
+
+        '''
+        list = []
+        
+        try:
+            file = open(fisier, "r", encoding="iso-8859-15")
+            for line in file:
+                list.append(line)
+            return list
+        except:
+            return ["The file was not found"]
+
     def change_the_word(self):
-##        cuvinte = { "wer" : 100,
-##                    "was" : 100,
-##                    "vor" : 100
-##                    }
-        cuvinte = ["wer", "wie", "vor"]
+        ''' (obj) -> None
 
-        id = random.randrange(0,3)
+        Pocedura care schimba cuvintul si scoate cuvintul din lista de silabe generate 
 
-        self.label1['text'] = cuvinte[id]
-        cuvinte.pop(id)
+        '''
+        if len(self.cuvinte) != 0:
+            id = random.randrange(0,len(self.cuvinte))
+            self.label1['text'] = self.cuvinte[id]
+            self.cuvinte.pop(id)
+        else:
+            self.label1['text'] = "Fertig !!! :-)"
+            
         return None
 
         
         tkinter.mainloop()
 
         
-##cuvinte = { "wer" : 100,
-##            "was" : 100,
-##            "vor" : 100
-##            }
-##if "vor" in cuvinte.keys():
-##    print("existing word")
-##else:
-##    print("add the word to the dictionary")
-
 
 if __name__ == "__main__" :
     my_gui = myGui()
